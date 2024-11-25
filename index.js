4aadef69d4882c444c33ea6d2f162ca4aa4a8f38
fahrinulyanisrina <fahrinulyanisrina@gmail.com>
2024-11-25 08:55:33 +0700

    feat: update index.js

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+//import module
 require("dotenv").config();
 require("express-async-errors");
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
+//import db file
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const songRoutes = require("./routes/songs");
-const playlistRoutes = require ("./routes/playlists");
+const playListRoutes = require("./routes/playlists");
 const searchRoutes = require("./routes/search");
 const app = express();
 
+//memanggil function connection
 connection()
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5174", // URL front-end
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+}));
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
-app.use("/api/login", authRoutes);
-app.use("/api/songs", songRoutes);
-app.use("/api/playlists", playlistRoutes);
+app.use("/api/users/", userRoutes);
+app.use("/api/login/", authRoutes);
+app.use("/api/songs/", songRoutes);
+app.use("/api/playlists/", playListRoutes);
 app.use("/api/", searchRoutes);
 
-const port = process.env.PORT ||8080;
-app.listen(port,console.log(`listening on port${port}..`))
-
+//port variable
+const port = process.env.PORT || 8080;
+//port-nya dinamis
+app.listen(port, console.log(`Listening on port${port}...`))
